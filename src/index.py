33e66cbe53d8d2cfd5a417e3dfa582ce9b97193d
Rupert Bryant-Greene <io@rupertbg.nz>
2020-12-09 15:26:53 +1300

    Add configurable cache count, fix reponses

--- src/index.py
+++ src/index.py
@@ -1,12 +1,14 @@
 import botocore
 import boto3
 import json
+from os import environ
 from functools import lru_cache
-s3_client = boto3.client("s3")
+s3_client = boto3.client('s3')
 
 MAPPINGS_FILENAME = 'mappings.json'
+CACHE_COUNT = os.environ.get('CACHE_COUNT') or 0
 
-@lru_cache
+@lru_cache(maxsize=CACHE_COUNT)
 def read_host_mappings():
     print('Reading host mapping file')
     with open(MAPPINGS_FILENAME) as mappings_file:
@@ -14,16 +16,20 @@ def read_host_mappings():
         return mappings
 
 
-@lru_cache
+@lru_cache(maxsize=CACHE_COUNT)
 def get_bucket_content(bucket, path):
-    print(f'Reading {path} from {bucket}')
+    if path.startswith('/'):
+        key = path[1:]
+    else:
+        key = path
+    print(f'Reading {key} from {bucket}')
     try:
         return s3_client.get_object(
             Bucket=bucket,
-            Key=path,
-        )['Body']
+            Key=key,
+        )
     except botocore.exceptions.ClientError as e:
-        print(e.response['Error']['Code'])
+        print(e)
 
 def lambda_handler(event, context):
     if 'headers' not in event or 'host' not in event['headers']:
@@ -40,23 +46,27 @@ def lambda_handler(event, context):
     if host in host_mappings.keys():
         bucket_name = host_mappings[host]
         content = get_bucket_content(bucket_name, path)
-        print(f'Returning content {host} -> {bucket_name}{path}')
-        return json.dumps({
-            "statusCode": 200,
-            "isBase64Encoded": 'image/' in content['ContentType'],
-            "headers": {
-                "Content-Type": content['ContentType'],
-            },
-            "body": content['Body'],
-        })
+        if content:
+            print(f'Returning content {host} -> {bucket_name}{path}')
+            content_body = content['Body'].read().decode('utf-8')
+            return {
+                'statusCode': 200,
+                'isBase64Encoded': 'ContentType' in content and 'image/' in content['ContentType'],
+                'headers': {
+                    'Content-Type': content['ContentType'],
+                },
+                'body': content_body,
+            }
+        else:
+            print(f'Content not found: {bucket_name}{path}')
     else:
         print(f'Host not found: {host}')
-        return json.dumps({
-            "statusCode": 404,
-            "statusDescription": "404 Not Found",
-            "isBase64Encoded": False,
-            "headers": {
-                "Content-Type": "text/html"
-            },
-            "body": "<p>Not found</p>"
-        })
+    return {
+        'statusCode': 404,
+        'statusDescription': '404 Not Found',
+        'isBase64Encoded': False,
+        'headers': {
+            'Content-Type': 'text/html'
+        },
+        'body': '<p>Not found</p>'
+    }
